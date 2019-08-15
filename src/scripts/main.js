ce7438c3d6d91e2ca5f000946b458f56d5e9dfc7
Matthew Ross <matthew.douglas.ross@gmail.com>
2019-08-15 09:36:14 -0500

    inital commit.

---
+++ src/scripts/main.js
@@ -0,0 +1,5 @@
+const message = "Your Webpack application is set up and ready to go. Please start writing code."
+
+document.querySelector("#container").innerHTML = `<h1>${message}</h1>`
+
+console.log(message)
